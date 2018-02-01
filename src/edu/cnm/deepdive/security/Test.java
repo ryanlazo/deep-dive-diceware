65449f3d1d4bb47166dc159315f99689020ee97c
ryanlazo <ryanlazo12@gmail.com>
2018-02-01 12:23:07 -0500

    Initial commit.

---
+++ src/edu/cnm/deepdive/security/Test.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive.security;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class Test {
+
+  public static void main(String[] args) throws FileNotFoundException, NoSuchAlgorithmException, IOException {
+    File file = new File("eff_large_wordlist.txt");
+    Diceware dw = new Diceware(file);
+    String[] passphrase = dw.generate (6);
+    System.out.println(Arrays.toString(passphrase));
+  }
+
+}
